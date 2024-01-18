5528e9b660698a6627bc0dbfb960cd1ef0aa0f4a
anushkavijay <91810898+anushkavijay@users.noreply.github.com>
2024-01-18 15:33:28 -0800

    added full files

---
+++ hello-world/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test-hello-world
+//
+//  Created by Anushka Vijay on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
