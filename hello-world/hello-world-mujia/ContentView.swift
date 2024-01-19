c6d8209be3f8bf2dd8e7aa458a5a55faa7bf3f3c
Saeri <saeri@Saeris-MacBook-Air.local>
2024-01-19 01:03:02 -0800

    added hello-world-mujia

---
+++ hello-world/hello-world-mujia/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by Saeri on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello-world/hello-world-mujia/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Saeri on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
