673c726bc922080bb8978424998a207133d2d24d
Karankumar Mageswaran <karan@169-231-215-201.wireless.ucsb.edu>
2024-01-19 12:54:26 -0800

    Added my hello-world

---
+++ hello-world/hello-world-swift-karan/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Karankumar Mageswaran on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
