9af62b379894a597c69d896f3ada451ad11bb563
Mike Wang <wangziming1424@gmail.com>
2024-01-23 17:35:15 -0800

    added new files in order to stash

---
+++ BeeFree/BeeFree/Features/Login/View/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  BeeFree
+//
+//  Created by Mike Wang on 1/23/24.
+//
+
+import Foundation
---
+++ BeeFree/BeeFree/Features/Login/View/LoginUtilities.swift
@@ -0,0 +1,8 @@
+//
+//  LoginUtilities.swift
+//  BeeFree
+//
+//  Created by Mike Wang on 1/23/24.
+//
+
+import Foundation
